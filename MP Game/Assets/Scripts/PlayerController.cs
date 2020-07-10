e35ed787d2b384e9f9979c6f9ed34ebb6c86efb7
wallnerma <wallnerma@cip.ifi.lmu.de>
2020-07-10 12:29:42 +0200

    Ordnerstruktur und Player der sliden kann

---
+++ MP Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput, 0) * speed;
+    }
+}
